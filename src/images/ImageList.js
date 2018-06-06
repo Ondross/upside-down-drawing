10bd664e5eed8e52bf9c77b3d65f104a8eeaa6c7
Andrew Heine <drewheine@gmail.com>
2018-06-05 17:55:22 -0700

    Image list and attributions

---
+++ src/images/ImageList.js
@@ -0,0 +1,95 @@
+
+export const imageList = [
+  {
+    src: "/static/images/80s.png",
+    attribution: "http://www.jeremyriad.com/blog/art/reading/i-love-the-80s-uk-colouring-book/",
+  },
+  {
+    src: "/static/images/ballerina1.jpg",
+    attribution: "https://www.boredart.com/wp-content/uploads/2014/08/Stunning-Ballerina-drawings-and-sketches-43.jpg",
+  },
+  {
+    src: "/static/images/ballerinaCoffee.jpg",
+    attribution: "https://www.boredart.com/wp-content/uploads/2014/08/Stunning-Ballerina-drawings-and-sketches-211.jpg",
+  },
+  {
+    src: "/static/images/ballroom-dancer.png",
+    attribution: "http://getdrawings.com/images/line-drawing-12.png",
+  },
+  {
+    src: "/static/images/butterflyDog.jpg",
+    attribution: "https://hative.com/wp-content/uploads/2013/09/dog-drawings/dog-drawing-5.jpg",
+  },
+  {
+    src: "/static/images/christianPartier.png",
+    attribution: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.notonthehighstreet.com%2Fsystem%2Fproduct_images%2Fimages%2F001%2F646%2F705%2Foriginal_1980s-colouring-in-book-for-grown-ups.jpg&imgrefurl=https%3A%2F%2Fwww.notonthehighstreet.com%2Fberylune%2Fproduct%2F1980s-colouring-in-book-for-grown-ups&docid=CiIizo1HlugyQM&tbnid=pyZlhJdoQNkUEM%3A&vet=10ahUKEwickbza5b3bAhXprlQKHR3qBoEQMwjnASgoMCg..i&w=900&h=900&bih=803&biw=1440&q=80s%20coloring%20book&ved=0ahUKEwickbza5b3bAhXprlQKHR3qBoEQMwjnASgoMCg&iact=mrc&uact=8",
+  },
+  {
+    src: "/static/images/climber.png",
+    attribution: "https://s-media-cache-ak0.pinimg.com/originals/3b/18/79/3b187981723ace19beaf504268bd7d73.jpg",
+  },
+  {
+    src: "/static/images/elephant.png",
+    attribution: "https://amanicworld.files.wordpress.com/2013/12/tumblr_mlwnkidh731rjz22oo1_1280.png",
+  },
+  {
+    src: "/static/images/frankZappa.jpg",
+    attribution: "https://i1.wp.com/multiversitystatic.s3.amazonaws.com/uploads/2014/08/Crumb-Zappa.jpg?fit=900%2C1259",
+  },
+  {
+    src: "/static/images/frog.png",
+    attribution: "https://www.bing.com/th?id=OIP.9YSHGtjRqC8HBLWA4ibaAwHaG_&pid=Api",
+  },
+  {
+    src: "/static/images/groot.png",
+    attribution: "https://www.drawingtutorials101.com/drawing-tutorials/Comic-Characters/Marvel-Comics/baby-groot/how-to-draw-Baby-Groot-step-0.png",
+  },
+  {
+    src: "/static/images/horse.jpg",
+    attribution: "https://i.pinimg.com/originals/76/aa/b7/76aab7f6cdbe9f023b0be659778e8e75.jpg",
+  },
+  {
+    src: "/static/images/jayAndBob.jpg",
+    attribution: "http://pre11.deviantart.net/3d3d/th/pre/i/2013/064/6/7/jay_and_silent_bob_by_nathankroll-d5x4kq7.jpg",
+  },
+  {
+    src: "/static/images/knight.jpg",
+    attribution: "http://pre02.deviantart.net/c401/th/pre/i/2012/048/9/d/norman_knight_by_pictishscout-d4q16mw.jpg",
+  },
+  {
+    src: "/static/images/kramer.jpeg",
+    attribution: "https://www.bing.com/th?id=OIP.RXuW1B4M1Y8vpNB8B8WZcQHaK9&pid=Api",
+  },
+  {
+    src: "/static/images/madScrapper.jpg",
+    attribution: "https://www.clipartqueen.com/image-files/dog-sketches-small-angry-dgo.jpg",
+  },
+  {
+    src: "/static/images/owlKid.jpg",
+    attribution: "https://femkegielkens.files.wordpress.com/2012/11/img_0724.jpg",
+  },
+  {
+    src: "/static/images/rcrumb.jpg",
+    attribution: "https://nyoobserver.files.wordpress.com/2015/10/crumblrg.jpg?quality=80",
+  },
+  {
+    src: "/static/images/releve.jpg",
+    attribution: "https://i.pinimg.com/736x/7b/d9/23/7bd923b3f3a0c6fcabffb79ab34eafad.jpg",
+  },
+  {
+    src: "/static/images/sadDog.png",
+    attribution: "https://www.bing.com/th?id=OIP.RyGCyRQDMmjjomws-ahbCgHaFS&pid=Api",
+  },
+  {
+    src: "/static/images/smugOwl.jpg",
+    attribution: "http://images.fineartamerica.com/images-medium-large/owl-sketch-barbara-stirrup.jpg",
+  },
+  {
+    src: "/static/images/stravinsky.png",
+    attribution: "https://upload.wikimedia.org/wikipedia/en/1/1c/Stravinsky_picasso.png",
+  },
+  {
+    src: "/static/images/thor.jpg",
+    attribution: "https://i.pinimg.com/originals/15/79/6b/15796bd45269babf15de00a6ea4d1745.jpg",
+  },
+]
--- src/images/index.jsx
+++ src/images/index.jsx
@@ -2,16 +2,13 @@ import S from 's-js'
 import * as Surplus from 'surplus'
 import SArray from 's-array'
 import data from 'surplus-mixin-data'
+import {imageList} from 'upsideDownDrawing/Images/imageList'
 
 require('upsideDownDrawing/Images/css/images.css')
 
 export default () => {
-  const images = [
-    "/static/images/stravinsky.png",
-    "/static/images/ballroom-dancer.png",
-  ],
-  shuffle = arr => arr.sort(() => Math.random() - 0.5),
-  indices = shuffle([...Array(images.length)].map((_, i) => i)),
+  const shuffle = arr => arr.sort(() => Math.random() - 0.5),
+  indices = shuffle([...Array(imageList.length)].map((_, i) => i)),
   imageIndex = S.data(0),
   flipped = S.data(false),
   decrement = () => {
@@ -33,6 +30,8 @@ export default () => {
     }
     imageIndex(next)
   },
+  searchTerm = S.data(''),
+  searchInput = <input type="text" fn={data(searchTerm)} className="search-input" placeholder="Search" />,
   ImageControls = () =>
     <div className="controls-container">
       <div className="controls-button" onClick={decrement}>
@@ -44,17 +43,48 @@ export default () => {
       <div className="controls-button" onClick={increment}>
         >
       </div>
-    </div>
+      {searchInput}
+    </div>,
+  ShuffledImage = () =>
+    <div className="image-wrapper">
+      <div className="image-container">
+        <img className={`drawing ${flipped() ? "" : "upside-down"}`} src={imageList[indices[imageIndex()]].src} />
+        <div className="attribution">
+          <a href={imageList[indices[imageIndex()]].attribution}>Attribution</a>
+        </div>
+      </div>
+    </div>,
+  SearchIFrame = () =>
+  // Scrolling set to no because there is a big when you click in image in the iframe after scrolling. We force you to click images and cycle through.
+  <iframe scrolling="no" className={`search-frame ${flipped() ? "" : "upside-down"}`} src={`https://www.bing.com/images/search?q=${searchTerm()}&go=Search&qs=ds&form=QBILPG`}></iframe>,
+  ImageDisplay = () => {
+    if (searchTerm()) {
+      return <SearchIFrame />
+    } else {
+      return <ShuffledImage />
+    }
+  }, keyCodeFunctions = {
+    38: flip,
+    40: flip,
+    37: decrement,
+    39: increment,
+  }
 
+  document.body.addEventListener('keydown', ({keyCode}) => {
+    if (document.activeElement === searchInput) {
+      return
+    }
+    if (keyCodeFunctions[keyCode]) {
+      keyCodeFunctions[keyCode]()
+      return
+    }
+    searchInput.focus()
+  })
 
   const root =
     <div className="drawing-container">
       <ImageControls />
-      <div className="image-wrapper">
-        <div className="image-container">
-          <img className={`drawing ${flipped() ? "" : "upside-down"}`} src={images[indices[imageIndex()]]} />
-        </div>
-      </div>
+      <ImageDisplay />
     </div>
 
   return root
