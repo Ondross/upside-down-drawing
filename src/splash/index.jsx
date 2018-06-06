3d3524008c1651030793cd1fff51865906e9f602
Andrew Heine <drewheine@gmail.com>
2018-06-05 18:28:05 -0700

    New copy.

--- src/splash/index.jsx
+++ src/splash/index.jsx
@@ -12,17 +12,19 @@ export default (next) => {
       <div className="app-header">
         Upside Down Drawing
       </div>
+      <div className="paragraph">
+        Upside down drawing is a method of tricking our brain into seeing the world as shapes rather than symbols. By looking at objects in an unfamiliar perspective, we trick our left-brain into ignoring what things <i>should</i> look like. This mental shift hels us focus on shapes, angles, and relationships.
+      </div>
+      <div className="paragraph">
+        Read more in Chapter 4 of <a href="http://drawright.com/">Drawing on the Right Side of the Brain</a> by Betty Edwards.
+      </div>
+      <div className="paragraph">
+        Before you start, turn on some instrumental music and make sure you won't be disturbed for at least 30 minutes. Try not to look at the drawings right side up until you're finished. I promise, you'll be surprised at the quality of your results!
+      </div>
       <div className="button" onClick={next}>
-        Start Drawing
-      </div>
-      <div className="paragraph">
-        Our goal is to see the world in shapes, rather than symbols. Upside down drawing helps trigger that mental shift.
-      </div>
-      <div className="paragraph">
-        This is inspired by Chapter 4 of <a href="http://drawright.com/">Drawing on the Right Side of the Brain</a> by Betty Edwards.
+        Go Upside Down
       </div>
     </div>
 
   return root
 }
-
