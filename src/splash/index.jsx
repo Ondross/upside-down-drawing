b6465a2249506c88b68b474174b97a4b018a70a9
Andrew Heine <drewheine@gmail.com>
2018-06-06 16:47:03 -0700

    Copy changes. Added subheader

--- src/splash/index.jsx
+++ src/splash/index.jsx
@@ -12,14 +12,17 @@ export default (next) => {
       <div className="app-header">
         Upside Down Drawing
       </div>
-      <div className="paragraph">
-        Upside down drawing is a trick for seeing the world as shapes rather than symbols. The part of your brain that categorizes everything is hard to ignore and not useful for drawing. Viewing images from an unfamiliar perspective disengages that part of your brain and allows you to observe what things actually <i>look like</i>.
+      <div className="app-sub-header">
+        See in shapes, not symbols
       </div>
       <div className="paragraph">
-        This site is a collection of fun upside images for drawing. To get started, tape some paper to your desk, get a pencil, and and make sure you're undisturbed for 30 minutes.
+        Upside down drawing is a trick for perceiving objects more accurately. By using an unfamiliar perspective, we disengage our logical left-brains in favor of our more observant, open-minded right-brains. When we forget labels, we see shapes and forms, and it becomes much easier to draw.
       </div>
       <div className="paragraph">
-        As you draw, pay attention to the shape of each line, not what it represents. Don't flip anything over until you're finished, and you'll be surprised at the quality of your results!
+        This site is a collection of fun upside images for drawing. To get started, tape some paper to your desk, get a pencil, and make sure you're not disturbed for 30 minutes.
+      </div>
+      <div className="paragraph">
+        As you draw, pay attention to the shape of each line, rather than what it represents. Don't flip anything over until you're finished, and you'll be surprised at the quality of your results!
       </div>
       <div className="button" onClick={next}>
         Start Drawing
